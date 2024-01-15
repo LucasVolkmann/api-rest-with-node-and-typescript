8452b1b7b0943834b49cda5bfb9368114045848b
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-01-15 16:07:53 -0300

    generate and validate jwt token

--- src/server/controllers/user/SignIn.ts
+++ src/server/controllers/user/SignIn.ts
@@ -5,7 +5,7 @@ import { Request, Response } from 'express';
 import { UserProvider } from '../../database/providers/user';
 import { StatusCodes } from 'http-status-codes';
 import { IUser } from '../../database/models';
-import { PasswordCrypto } from '../../shared/services';
+import { PasswordCrypto, jwtService } from '../../shared/services';
 
 
 interface IBodyProps extends Omit<IUser, 'id' | 'username'> {
@@ -25,13 +25,22 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
 
   const result = await UserProvider.getByEmail(reqEmail);
 
-  
+
   if (result instanceof Error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: { default: result.message } 
+      errors: { default: result.message }
     });
   } else if (result && await PasswordCrypto.verifyPassword(reqPassword, result.password)) {
-    return res.status(StatusCodes.OK).json({ accessToken: 'mock.access.token' });
+    
+    const accessToken = jwtService.sign({ uid: result.id });
+
+    if(accessToken === 'JWT_SECRET_NOT_FOUND'){
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: { default: 'Error while generating new token.' }
+      });
+    }
+
+    return res.status(StatusCodes.OK).json({ accessToken });
   } else {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
--- src/server/shared/middleware/EnsureAuthenticated.ts
+++ src/server/shared/middleware/EnsureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { jwtService } from '../services';
 
 
 export const ensureAuthenticated: RequestHandler = (req, res, next) => {
@@ -13,14 +14,19 @@ export const ensureAuthenticated: RequestHandler = (req, res, next) => {
   }
 
   const [ type, token ] = authorization.split(' ');
-
-  if(type === 'Bearer' && token === 'test.test.test') {
-    return next();
-  } else {
+  const dataToken = jwtService.verify(token);
+  if(type !== 'Bearer' || dataToken === 'INVALID_TOKEN') {
     return res.status(StatusCodes.UNAUTHORIZED).json({
-      errors: { default: 'Not authenticated.' }
+      errors: { default: 'Invalid token.' }
+    });
+  }
+  if(dataToken === 'JWT_SECRET_NOT_FOUND'){
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: 'Error while verifying token.' }
     });
   }
 
+  req.headers.userId = dataToken.uid.toString();
 
+  return next();
 };
