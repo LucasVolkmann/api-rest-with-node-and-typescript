ce0936a8a1aeed25112912decd9ed47eaa670de9
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-01-16 20:03:59 -0300

    setup for postgres container and production environment

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -21,12 +21,44 @@ export const development: Knex.Config = {
   }
 };
 
+export const developmentDockerPostgres: Knex.Config = {
+  client: 'pg',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: false
+  }
+};
+
 export const test: Knex.Config = {
   ...development,
   connection: ':memory:',
 };
 
 export const production: Knex.Config = {
-  ...development
+  client: 'pg',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: { rejectUnauthorized: false }
+  },
 };
 
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,11 +1,18 @@
 import { knex } from 'knex';
-import { development, production, test } from './Environment';
+import 'dotenv/config';
+import pg from 'pg';
 
+import { development, developmentDockerPostgres, production, test } from './Environment';
+
+if(process.env.NODE_ENV === 'dev_doc_pg' || process.env.NODE_ENV === 'production'){
+  pg.types.setTypeParser(pg.types.builtins.INT8, 'text', parseInt);
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {
     case 'production': return production;
     case 'test': return test;
+    case 'dev_doc_pg': return developmentDockerPostgres;
 
     default: return development;
   }
